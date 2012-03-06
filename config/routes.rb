7692f6ba55e786bc19780b00151995cd888b0207
Igor Pertsav <pertsavigorstray@gmail.com>
2012-03-06 19:23:08 +0300

    devise test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 DeviseTest::Application.routes.draw do
+  devise_for :users
+
+  root :to => 'application#new'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
